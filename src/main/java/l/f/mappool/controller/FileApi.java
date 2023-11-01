8269d19f65ba517a07b46cf7f3f93d1b5af702ca
spring <365246692@qq.com>
2023-11-01 15:34:37 +0800

    bug

--- src/main/java/l/f/mappool/controller/FileApi.java
+++ src/main/java/l/f/mappool/controller/FileApi.java
@@ -209,7 +209,7 @@ public class FileApi {
         return response.getOutputStream();
     }
 
-    @Open(bot = true)
+    @Open()
     @GetMapping("local/{type}/{bid}")
     public String getLocalPath(@PathVariable Long bid, @PathVariable String type) throws IOException {
         var atype = switch (type) {
@@ -227,7 +227,7 @@ public class FileApi {
 
     static int SUM = 0;
 
-    @Open(bot = true, pub = false)
+    @Open()
     @GetMapping("local/async/{bid}")
     public String getLocalPathAsync(@PathVariable Long bid, @RequestHeader("SET_ID") Long sid) throws IOException {
         log.info("异步任务: 开始下载 [{}]", sid);
