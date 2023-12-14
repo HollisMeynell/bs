53850798a751bad5e6b2fc0541be659e4fb07a53
spring <365246692@qq.com>
2023-12-14 20:40:17 +0800

    update

--- src/main/java/l/f/mappool/properties/BeatmapSelectionProperties.java
+++ src/main/java/l/f/mappool/properties/BeatmapSelectionProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -26,5 +27,5 @@ public class BeatmapSelectionProperties {
     /**
      * 管理员
      */
-    List<Long> adminUsers;
+    List<Long> adminUsers = new ArrayList<>();
 }
