2e4bf42509596de031e7412f99bbda6300b3024e
spring <365246692@qq.com>
2024-04-19 21:12:36 +0800

    更新

--- src/main/java/l/f/mappool/repository/file/OsuFileLogRepository.java
+++ src/main/java/l/f/mappool/repository/file/OsuFileLogRepository.java
@@ -1,11 +1,11 @@
 package l.f.mappool.repository.file;
 
+import jakarta.transaction.Transactional;
 import l.f.mappool.entity.file.OsuFileRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
-import java.beans.Transient;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +17,7 @@ public interface OsuFileLogRepository extends JpaRepository<OsuFileRecord, Long>
 
     Optional<OsuFileRecord> findOsuFileRecordByBid(Long bid);
 
-    @Transient
+    @Transactional
     void deleteAllBySid(long sid);
 
     @Query("select count(osu)from OsuFileRecord osu")
